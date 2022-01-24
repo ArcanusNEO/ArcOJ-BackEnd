e70c0c62bab0c04661031cb2a4485915cba47e0d
ArcanusNEO <lucasding@foxmail.com>
2022-01-24 22:11:21 +0800

    .

--- routes/contest.js
+++ routes/contest.js
@@ -1,26 +1,4 @@
-let express = require('express')
-let router = express.Router()
-let hsc = require('../config/http-status-code')
-let db = require('../utils/database')
+let { getAll, router } = require('./problemset')
 let lc = require('./midwares/login-check')
-let mc = require('./midwares/member-check')
-
-router.get('/', lc, async (req, res) => {
-  let uid = req.tokenAcc.uid
-  let query = `SELECT "problemset"."psid" AS "id", "title" AS "name" FROM "problemset_user" INNER JOIN "problemset" ON "problemset"."psid" = "problemset_user"."psid" WHERE "uid" = $1 AND "type" = 'contest' ORDER BY "problemset"."psid" DESC`
-  let ret = (await db.query(query, [uid])).rows
-  return res.status(hsc.ok).json({ assignments: ret })
-})
-
-router.get('/id/:psid(\\d+)', lc,
-  async (req, res, next) => {
-    return (mc['problemset'](req.tokenAcc.uid, req.params.psid)(req, res, next))
-  },
-  async (req, res) => {
-    let query = 'SELECT "problemset"."title" AS "name", "problemset"."description" AS "description", "private", "during", "course"."title" AS "courseName" FROM "problemset" LEFT JOIN "course" ON "problemset"."cid" = "course"."cid" WHERE "psid" = $1 AND "course"."visiable" = TRUE'
-    let ret = (await db.query(query, req.params.psid)).rows[0]
-    if (ret) return res.status(hsc.ok).json(ret)
-    else return res.sendStatus(hsc.unauthorized)
-  })
-
+router.get('/', lc, getAll('contest'))
 module.exports = router
--- routes/problemset.js
+++ routes/problemset.js
@@ -1,17 +1,18 @@
 let express = require('express')
 let router = express.Router()
 let hsc = require('../config/http-status-code')
-let db = require('./database')
-let lc = require('../routes/midwares/login-check')
-let mc = require('../routes/midwares/member-check')
+let db = require('../utils/database')
+let lc = require('./midwares/login-check')
+let mc = require('./midwares/member-check')
 
-router.get('/', lc, async (req, res) => {
-  let uid = req.tokenAcc.uid
-  let query = `SELECT "problemset"."psid" AS "id", "title" AS "name" FROM "problemset_user" INNER JOIN "problemset" ON "problemset"."psid" = "problemset_user"."psid" WHERE "uid" = $1 AND "type" = 'contest' ORDER BY "problemset"."psid" DESC`
-  let ret = (await db.query(query, [uid])).rows
-  return res.status(hsc.ok).json({ ret })
-})
-
+const getAll = (property) => {
+  return async (req, res) => {
+    let uid = req.tokenAcc.uid, ret
+    let query = `SELECT "problemset"."psid" AS "id", "title" AS "name" FROM "problemset_user" INNER JOIN "problemset" ON "problemset"."psid" = "problemset_user"."psid" WHERE "uid" = $1 AND "type" = 'assignment' ORDER BY "problemset"."psid" DESC`
+    ret[property] = (await db.query(query, [uid])).rows
+    return res.status(hsc.ok).json(ret)
+  }
+}
 router.get('/id/:psid(\\d+)', lc,
   async (req, res, next) => {
     return (mc['problemset'](req.tokenAcc.uid, req.params.psid)(req, res, next))
@@ -23,4 +24,4 @@ router.get('/id/:psid(\\d+)', lc,
     else return res.sendStatus(hsc.unauthorized)
   })
 
-module.exports = router
+module.exports = { getAll, router }
