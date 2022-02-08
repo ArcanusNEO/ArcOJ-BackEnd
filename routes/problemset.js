c10d738fbd868028b8a26e011d8463ec354a894a
ArcanusNEO <lucasding@foxmail.com>
2022-02-08 22:41:20 +0800

    .

--- routes/problemset.js
+++ routes/problemset.js
@@ -8,8 +8,12 @@ let mc = require('./midwares/member-check')
 const getAll = (property) => {
   return async (req, res) => {
     let uid = req.tokenAcc.uid, ret = {}
-    let query = `SELECT "problemset"."psid" AS "id", "title" AS "name" FROM "problemset_user" INNER JOIN "problemset" ON "problemset"."psid" = "problemset_user"."psid" WHERE "uid" = $1 AND "type" = ${property} ORDER BY "problemset"."psid" DESC`
-    ret[property] = (await db.query(query, [uid])).rows
+    let cid = parseInt(req.query.cid)
+    let param = []
+    let query = `SELECT "problemset"."psid" AS "id", "title" AS "name" FROM "problemset_user" INNER JOIN "problemset" ON "problemset"."psid" = "problemset_user"."psid" WHERE "uid" = ${param.push(uid)} AND "type" = ${property}`
+    if (cid > 0) query += ` AND "cid" = ${param.push(cid)}`
+    query += ' ORDER BY "problemset"."psid" DESC'
+    ret[property] = (await db.query(query, param)).rows
     return res.status(hsc.ok).json(ret)
   }
 }
