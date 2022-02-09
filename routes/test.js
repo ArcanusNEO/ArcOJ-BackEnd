198535295ff9fda37d3eb2dccf50e5e7dc9b5e5b
ArcanusNEO <lucasding@foxmail.com>
2022-02-09 14:55:03 +0800

    .

--- routes/test.js
+++ routes/test.js
@@ -10,7 +10,7 @@ let fc = require('./midwares/form-check')
 
 router.get('/', async (req, res) => {
   let query = 'SELECT "problemset"."title" AS "name", "problemset"."description" AS "description", "private", "during", "course"."title" AS "courseName" FROM "problemset" LEFT JOIN "course" ON "problemset"."cid" = "course"."cid" WHERE "psid" = $1 AND "course"."visiable" = TRUE'
-  let ret = (await db.query(query, [req.params.psid])).rows[0]
+  let ret = (await db.query(query, [req.query.psid])).rows[0]
   if (ret) return res.status(hsc.ok).json(ret)
   else return res.sendStatus(hsc.unauthorized)
 })
