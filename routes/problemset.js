2059fe7893ac88e2de4af5cbaae411d6dcf1f4dc
ArcanusNEO <lucasding@foxmail.com>
2022-02-09 14:47:21 +0800

    .

--- routes/problemset.js
+++ routes/problemset.js
@@ -11,7 +11,7 @@ const getAll = (property) => {
     let cid = parseInt(req.query.cid)
     let param = []
     let query = `SELECT "problemset"."psid" AS "id", "title" AS "name" FROM "problemset_user" INNER JOIN "problemset" ON "problemset"."psid" = "problemset_user"."psid" WHERE "uid" = $${param.push(uid)} AND "type" = '${property}'`
-    if (cid > 0) query += ` AND "cid" = '$${param.push(cid)}'`
+    if (cid > 0) query += ` AND "cid" = $${param.push(cid)}`
     query += ' ORDER BY "problemset"."psid" DESC'
     ret[property] = (await db.query(query, param)).rows
     return res.status(hsc.ok).json(ret)
