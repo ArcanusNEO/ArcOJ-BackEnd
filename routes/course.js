918a2a1a2e7de3e5305440fa65102f8b5663b418
ArcanusNEO <lucasding@foxmail.com>
2022-02-09 20:27:05 +0800

    。

--- routes/course.js
+++ routes/course.js
@@ -7,7 +7,7 @@ let mc = require('./midwares/member-check')
 
 router.get('/', lc, async (req, res) => {
   let uid = req.tokenAcc.uid
-  let query = 'SELECT "course"."cid" AS "id", "title" AS "name" FROM "course_user" INNER JOIN "course" ON "course"."cid" = "course_user"."cid" WHERE "uid" = $1 AND "visiable" = TRUE ORDER BY "course"."cid" DESC'
+  let query = 'SELECT "course"."cid" AS "id", "title" AS "name" FROM "course_user" INNER JOIN "course" ON "course"."cid" = "course_user"."cid" WHERE "uid" = $1 AND "visiable" ORDER BY "course"."cid" DESC'
   let ret = (await db.query(query, [uid])).rows
   return res.status(hsc.ok).json({ courses: ret })
 })
@@ -17,7 +17,7 @@ router.get('/id/:cid(\\d+)', lc,
     return (mc['course'](req.tokenAcc.uid, req.params.cid)(req, res, next))
   },
   async (req, res) => {
-    let query = 'SELECT "title" AS "name", "teacher" AS "teachers", "number", "description", "semester" FROM "course" WHERE "cid" = $1 AND "visiable" = TRUE'
+    let query = 'SELECT "title" AS "name", "teacher" AS "teachers", "number", "description", "semester" FROM "course" WHERE "cid" = $1 AND "visiable"'
     let ret = (await db.query(query, [req.params.cid])).rows[0]
     if (ret) return res.status(hsc.ok).json(ret)
     else return res.sendStatus(hsc.unauthorized)
