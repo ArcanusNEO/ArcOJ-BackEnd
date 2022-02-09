5fbf92eb0549de2964b67a6e02e0838d08d1c099
ArcanusNEO <lucasding@foxmail.com>
2022-02-09 14:54:00 +0800

    .

--- routes/test.js
+++ routes/test.js
@@ -9,15 +9,10 @@ let tokenUtils = require('../utils/token')
 let fc = require('./midwares/form-check')
 
 router.get('/', async (req, res) => {
-  let uid = 3, ret = {}
-  let cid = parseInt(req.query.cid)
-  let param = []
-  let property = 'assignment'
-  let query = `SELECT "problemset"."psid" AS "id", "title" AS "name" FROM "problemset_user" INNER JOIN "problemset" ON "problemset"."psid" = "problemset_user"."psid" WHERE "uid" = $${param.push(uid)} AND "type" = '${property}'`
-  if (cid > 0) query += ` AND "cid" = $${param.push(cid)}`
-  query += ' ORDER BY "problemset"."psid" DESC'
-  ret[property] = (await db.query(query, param)).rows
-  return res.status(hsc.ok).json(ret)
+  let query = 'SELECT "problemset"."title" AS "name", "problemset"."description" AS "description", "private", "during", "course"."title" AS "courseName" FROM "problemset" LEFT JOIN "course" ON "problemset"."cid" = "course"."cid" WHERE "psid" = $1 AND "course"."visiable" = TRUE'
+  let ret = (await db.query(query, [req.params.psid])).rows[0]
+  if (ret) return res.status(hsc.ok).json(ret)
+  else return res.sendStatus(hsc.unauthorized)
 })
 
 module.exports = router
