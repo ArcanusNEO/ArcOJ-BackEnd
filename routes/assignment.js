6945ab76e326deedc40eed62194b32e66695807c
ArcanusNEO <lucasding@foxmail.com>
2022-02-10 21:38:12 +0800

    .

--- routes/assignment.js
+++ routes/assignment.js
@@ -1,8 +1,19 @@
-let { getAll, get, getOpen, router } = require('./problemset')
+let { get, getOpen, router } = require('./problemset')
 let lc = require('./midwares/login-check')
-router.get('/', lc, getAll('assignment'))
-router.get('/open', lc, getOpen('assignment'))
+router.get('/', lc,
+  async (req, res, next) => {
+    req.params.cid = 0
+    return next()
+  },
+  get('assignment'))
+router.get('/open', lc,
+  async (req, res, next) => {
+    req.params.cid = 0
+    return next()
+  },
+  getOpen('assignment'))
 router.get('/global', lc, get('assignment'))
+router.get('/global/open', lc, getOpen('assignment'))
 router.get('/course(s)?/:cid(\\d+)', lc,
   async (req, res, next) => {
     return (mc['course'](req.tokenAcc.uid, req.params.cid)(req, res, next))
--- routes/problemset.js
+++ routes/problemset.js
@@ -5,23 +5,15 @@ let db = require('../utils/database')
 let lc = require('./midwares/login-check')
 let mc = require('./midwares/member-check')
 
-const getAll = (property) => {
-  return async (req, res) => {
-    let uid = req.tokenAcc.uid, param = []
-    let query = `SELECT "problemset"."psid" AS "id", "title" AS "name", LOWER("during")::TIMESTAMPTZ AS "begin", UPPER("during")::TIMESTAMPTZ AS "end" FROM "problemset_user" INNER JOIN "problemset" ON "problemset"."psid" = "problemset_user"."psid" WHERE "uid" = $${param.push(uid)} AND "type" = '${property}' ORDER BY "problemset"."psid" DESC`
-    let ret = (await db.query(query, param)).rows
-    return res.status(hsc.ok).json(ret)
-  }
-}
-
 const get = (property) => {
   return async (req, res) => {
     let uid = req.tokenAcc.uid
     let cid = parseInt(req.params.cid)
     let param = []
     let query = `SELECT "problemset"."psid" AS "id", "title" AS "name", LOWER("during")::TIMESTAMPTZ AS "begin", UPPER("during")::TIMESTAMPTZ AS "end" FROM "problemset_user" INNER JOIN "problemset" ON "problemset"."psid" = "problemset_user"."psid" WHERE "uid" = $${param.push(uid)} AND "type" = '${property}'`
-    if (cid > 0) query += ` AND "cid" = $${param.push(cid)}`
-    else query += ' AND "cid" ISNULL'
+    if (cid > 0) query += ` AND "cid" = $${param.push(cid)}` // 返回 course = cid
+    else if (cid === 0) { /* 返回所有的 */ }
+    else query += ' AND "cid" ISNULL' // 返回 global
     query += ' ORDER BY "problemset"."psid" DESC'
     let ret = (await db.query(query, param)).rows
     return res.status(hsc.ok).json(ret)
@@ -34,10 +26,11 @@ const getOpen = (property) => {
     let cid = parseInt(req.params.cid)
     let param = []
     let query = 'SELECT "problemset"."psid" AS "id", "problemset"."title" AS "name", LOWER("during")::TIMESTAMPTZ AS "begin", UPPER("during")::TIMESTAMPTZ AS "end"'
-    if (cid > 0) query += ', "course"."title" AS "courseName"'
+    if (cid >= 0) query += ', "course"."title" AS "courseName"'
     query += ' FROM "problemset" INNER JOIN "problemset_user" ON "problemset"."psid" = "problemset_user"."psid"'
-    if (cid > 0) query += ` INNER JOIN "course" ON "problemset"."cid" = "course"."cid" WHERE "problemset"."cid" = $${param.push(cid)}`
-    else query += ' WHERE "problemset"."cid" ISNULL'
+    if (cid > 0) query += ` INNER JOIN "course" ON "problemset"."cid" = "course"."cid" WHERE "problemset"."cid" = $${param.push(cid)}` // 返回 course = cid
+    else if (cid === 0) query += ` LEFT JOIN "course" ON "problemset"."cid" = "course"."cid" WHERE TRUE` // 返回所有的
+    else query += ' WHERE "problemset"."cid" ISNULL' // 返回 global
     query += ` AND NOW()::TIMESTAMPTZ <@ "problemset"."during" AND "problemset_user"."uid" = $${param.push(uid)} AND "problemset"."type" = '${property}' ORDER BY "end" ASC`
     let ret = (await db.query(query, param)).rows
     return res.status(hsc.ok).json(ret)
@@ -54,4 +47,4 @@ router.get('/id/:psid(\\d+)', lc,
     return res.status(hsc.ok).json(ret)
   })
 
-module.exports = { getAll, get, getOpen, router }
+module.exports = { get, getOpen, router }
