03766f55ce11e545c9f78e850a5a9c1222b1d373
ArcanusNEO <lucasding@foxmail.com>
2022-02-10 21:39:08 +0800

    .

--- routes/contest.js
+++ routes/contest.js
@@ -1,8 +1,19 @@
-let { getAll, get, getOpen, router } = require('./problemset')
+let { get, getOpen, router } = require('./problemset')
 let lc = require('./midwares/login-check')
-router.get('/', lc, getAll('contest'))
-router.get('/open', lc, getOpen('contest'))
+router.get('/', lc,
+  async (req, res, next) => {
+    req.params.cid = 0
+    return next()
+  },
+  get('contest'))
+router.get('/open', lc,
+  async (req, res, next) => {
+    req.params.cid = 0
+    return next()
+  },
+  getOpen('contest'))
 router.get('/global', lc, get('contest'))
+router.get('/global/open', lc, getOpen('contest'))
 router.get('/course(s)?/:cid(\\d+)', lc,
   async (req, res, next) => {
     return (mc['course'](req.tokenAcc.uid, req.params.cid)(req, res, next))
