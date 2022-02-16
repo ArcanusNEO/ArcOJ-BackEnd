b3cb01b7c1411eb6e14776bc0b371d4b46dc55c1
ArcanusNEO <lucasding@foxmail.com>
2022-02-16 18:20:02 +0800

    .

--- routes/midwares/form-check.js
+++ routes/midwares/form-check.js
@@ -51,7 +51,6 @@ const email = (req, pos, item) => {
   try {
     // if (req[pos][item].indexOf('@') === -1) req[pos][item] += '@mail.nankai.edu.cn'
     req[pos][item] = req[pos][item].toLowerCase()
-    console.log(req[pos][item])
     if (!/^[0-9a-z_-]+@[0-9a-z_-]+(\.[0-9a-z_-]+)+$/igs.test(req[pos][item])) throw Error('Invalid email address')
     // || !/@(.+\.)?nankai\.edu\.cn$/igs.test(req[pos][item])) 
   } catch (err) {
