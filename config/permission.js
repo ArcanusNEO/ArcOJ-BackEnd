d8398a43b28485a3663d8025a60cdb04492fad1c
ArcanusNEO <lucasding@foxmail.com>
2022-04-07 21:21:30 +0800

    Fix. Update permission document

--- config/permission.js
+++ config/permission.js
@@ -1,7 +1,7 @@
 module.exports = {
   'master': 1,                      // 最高权限位，一些奇奇怪怪的权限会检查
   'editGroup': 2,
-  'getShareCode': 3,                // 还未实现
+  'getShareCode': 3,                // 还未实现，允不允许普通用户查看别人的公开代码，尚在讨论必要性
   'changeProfile': 4,
   'submitCode': 5,                  // 评测权限
   'editGlobalProblem': 6,
@@ -13,18 +13,19 @@ module.exports = {
   'rejudgeLocalProblem': 12,
   'postMessage': 13,
   'postAnnouncement': 14,
-  'getGlobalCode': 15,              // 指即便code.share为false并且正在比赛也能随意查看代码
-  'getLocalCode': 16,               // 指即便code.share为false并且正在比赛也能随意查看代码
-  'getGlobalJudgeInfo': 17,         // 指所有用户的
-  'getLocalJudgeInfo': 18,          // 指所有用户的
-  'getJudgeInfo': 19,               // 指自己的
+  'getGlobalCode': 15,              // 已废弃
+  'getLocalCode': 16,               // 已废弃
+  'getGlobalJudgeInfo': 17,         // 已废弃
+  'getLocalJudgeInfo': 18,          // 已废弃
+  'getJudgeInfo': 19,               // 指所有的
   'forkGlobalProblem': 20,
   'forkLocalProblem': 21,
   'joinCourse': 22,
   'joinProblemset': 23
 }
-// 大部分权限都用于管理页，普通api下的特权是hack行为
+// 大部分权限都用于管理页
 
+// 设想中的权限
 //                               Default User   Administrator   Read Only
 // master                        0              0               0
 // editGroup                     0              0               0
@@ -44,7 +45,7 @@ module.exports = {
 // getLocalCode                  0              1               0
 // getGlobalJudgeInfo            0              0               0
 // getLocalJudgeInfo             0              1               0
-// getJudgeStatus                1              1               0
+// getJudgeInfo                  1              1               0
 // forkGlobalProblem             0              1               0
 // forkLocalProblem              0              1               0
 // joinCourse                    1              1               0
