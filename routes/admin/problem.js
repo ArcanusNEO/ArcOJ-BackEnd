17f6d863ae768e3bbba562c78b278c68081b793a
ArcanusNEO <lucasding@foxmail.com>
2022-04-09 21:21:49 +0800

    Fix. Dup api

--- routes/admin/problem.js
+++ routes/admin/problem.js
@@ -269,6 +269,19 @@ router.get('/id/:pid(\\d+)/statistics', lc, pidPermChk,
     let pid = parseInt(req.params.pid)
     let query = 'SELECT DISTINCT ON ("solution"."uid") "solution"."pid", "problem"."title", "solution"."sid", "solution"."uid", "user"."email" AS "email", "user"."nickname", "user"."realname", "solution"."score", "solution"."status_id" AS "status", "solution"."lang_id" AS "lang", "solution"."code_size" AS "codeSize (B)", "solution"."run_time" AS "time (ms)", "solution"."run_memory" AS "memory (KiB)" FROM "problem" INNER JOIN "solution" ON "problem"."pid" = "solution"."pid" INNER JOIN "user" ON "solution"."uid" = "user"."uid" WHERE "solution"."pid" = $1 ORDER BY "solution"."uid" ASC, "solution"."score" DESC, "solution"."sid" DESC'
     let ret = (await db.query(query, [pid])).rows
+    for (let row of ret) {
+      row.status = jsc.codeMsg[row.status]
+      row.lang = langMap.idLang[row.lang]
+    }
+    res.status(hsc.ok).json(ret)
+  }
+)
+
+router.get('/id/:pid(\\d+)/code(s)?', lc, pidPermChk,
+  async (req, res) => {
+    let pid = parseInt(req.params.pid)
+    let query = 'SELECT DISTINCT ON ("solution"."uid") "solution"."pid", "problem"."title", "solution"."sid", "solution"."uid", "user"."email" AS "email", "user"."nickname", "user"."realname", "solution"."score", "solution"."status_id" AS "status", "solution"."lang_id" AS "lang", "solution"."code_size" AS "codeSize (B)", "solution"."run_time" AS "time (ms)", "solution"."run_memory" AS "memory (KiB)" FROM "problem" INNER JOIN "solution" ON "problem"."pid" = "solution"."pid" INNER JOIN "user" ON "solution"."uid" = "user"."uid" WHERE "solution"."pid" = $1 ORDER BY "solution"."uid" ASC, "solution"."score" DESC, "solution"."sid" DESC'
+    let ret = (await db.query(query, [pid])).rows
     await fs.ensureDir(dataPath.temp)
     let solTmpDir = path.resolve(dataPath.temp, `${pid}-sol`)
     await fs.ensureDir(solTmpDir)
@@ -287,7 +300,6 @@ router.get('/id/:pid(\\d+)/statistics', lc, pidPermChk,
       fs.unlink(solSetTmp, (fserr) => { console.error(fserr) })
       fs.remove(solTmpDir, (fserr) => { console.error(fserr) })
     })
-    // res.status(hsc.ok).json(ret)
   }
 )
 
