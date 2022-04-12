ff78afbfaedb8e80aa3a819f92999d5c4dc17d4c
ArcanusNEO <lucasding@foxmail.com>
2022-04-12 23:02:48 +0800

    .

--- routes/admin/rejudge.mjs
+++ routes/admin/rejudge.mjs
@@ -6,7 +6,7 @@ import db from '../../utils/database.mjs'
 import mtc from '../midwares/maintainer-check.mjs'
 import pc from '../midwares/permission-check.mjs'
 import judgecore from '../../utils/judge.mjs'
-const { getProblemStructure, getSolutionStructure } = judgecore
+const { judge, getSolutionStructure } = judgecore
 import languageExtension from '../../config/lang-ext.mjs'
 import jsc from '../../config/judge-status-code.mjs'
 import fs from 'fs-extra'
