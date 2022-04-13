834c32490fed6d37f85514694ec557986691da93
ArcanusNEO <lucasding@foxmail.com>
2022-04-13 13:36:14 +0800

    Feat. Maintainer check returning boolean

---
+++ routes/midwares/maintainer-check-ret-bool.mjs
@@ -0,0 +1,37 @@
+import db from '../../utils/database.mjs'
+
+const problem = (uid, pid) => {
+  let query = 'SELECT "owner_id" = $1 AS "own" FROM "problem" WHERE "pid" = $2'
+  let ret = (await db.query(query, [uid, pid])).rows[0]
+  if (!ret) return false
+  if (ret.own) return true
+  query = 'SELECT "uid" FROM "problem_maintainer" WHERE "uid" = $1 AND "pid" = $2 LIMIT 1'
+  ret = (await db.query(query, [uid, pid])).rows[0]
+  return (ret ? true : false)
+}
+
+const problemset = (uid, psid) => {
+  let query = 'SELECT "owner_id" = $1 AS "own" FROM "problemset" WHERE "psid" = $2'
+  let ret = (await db.query(query, [uid, psid])).rows[0]
+  if (!ret) return false
+  if (ret.own) return true
+  query = 'SELECT "uid" FROM "problemset_maintainer" WHERE "uid" = $1 AND "psid" = $2 LIMIT 1'
+  ret = (await db.query(query, [uid, psid])).rows[0]
+  return (ret ? true : false)
+}
+
+const course = (uid, cid) => {
+  let query = 'SELECT "owner_id" = $1 AS "own" FROM "course" WHERE "cid" = $2'
+  let ret = (await db.query(query, [uid, cid])).rows[0]
+  if (!ret) return false
+  if (ret.own) return true
+  query = 'SELECT "uid" FROM "course_maintainer" WHERE "uid" = $1 AND "cid" = $2 LIMIT 1'
+  ret = (await db.query(query, [uid, cid])).rows[0]
+  return (ret ? true : false)
+}
+
+export default {
+  problem,
+  problemset,
+  course
+}
--- routes/midwares/maintainer-check.mjs
+++ routes/midwares/maintainer-check.mjs
@@ -1,41 +1,23 @@
-import db from '../../utils/database.mjs'
+import mtcrb from './maintainer-check-ret-bool.mjs'
 import hsc from '../../config/http-status-code.mjs'
 
 const problem = (uid, pid) => {
   return async (req, res, next) => {
-    let query = 'SELECT "owner_id" = $1 AS "own" FROM "problem" WHERE "pid" = $2'
-    let ret = (await db.query(query, [uid, pid])).rows[0]
-    if (!ret) return res.sendStatus(hsc.forbidden)
-    if (ret.own) return next()
-    query = 'SELECT "uid" FROM "problem_maintainer" WHERE "uid" = $1 AND "pid" = $2 LIMIT 1'
-    ret = (await db.query(query, [uid, pid])).rows[0]
-    if (ret) return next()
+    if (mtcrb.problem(uid, pid)) return next()
     return res.sendStatus(hsc.forbidden)
   }
 }
 
 const problemset = (uid, psid) => {
   return async (req, res, next) => {
-    let query = 'SELECT "owner_id" = $1 AS "own" FROM "problemset" WHERE "psid" = $2'
-    let ret = (await db.query(query, [uid, psid])).rows[0]
-    if (!ret) return res.sendStatus(hsc.forbidden)
-    if (ret.own) return next()
-    query = 'SELECT "uid" FROM "problemset_maintainer" WHERE "uid" = $1 AND "psid" = $2 LIMIT 1'
-    ret = (await db.query(query, [uid, psid])).rows[0]
-    if (ret) return next()
+    if (mtcrb.problemset(uid, psid)) return next()
     return res.sendStatus(hsc.forbidden)
   }
 }
 
 const course = (uid, cid) => {
   return async (req, res, next) => {
-    let query = 'SELECT "owner_id" = $1 AS "own" FROM "course" WHERE "cid" = $2'
-    let ret = (await db.query(query, [uid, cid])).rows[0]
-    if (!ret) return res.sendStatus(hsc.forbidden)
-    if (ret.own) return next()
-    query = 'SELECT "uid" FROM "course_maintainer" WHERE "uid" = $1 AND "cid" = $2 LIMIT 1'
-    ret = (await db.query(query, [uid, cid])).rows[0]
-    if (ret) return next()
+    if (mtcrb.course(uid, cid)) return next()
     return res.sendStatus(hsc.forbidden)
   }
 }
