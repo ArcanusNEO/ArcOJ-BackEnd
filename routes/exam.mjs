c49f966b9e890a910226494aec0c0a3fe0065897
ArcanusNEO <lucasding@foxmail.com>
2022-04-28 15:18:36 +0800

    .

--- routes/exam.mjs
+++ routes/exam.mjs
@@ -45,7 +45,7 @@ router.get('/id/:psid(\\d+)/rank', lc,
     let psid = parseInt(req.params.psid)
     let query = 'SELECT "pid", "title" FROM "problem" WHERE "psid" = $1 ORDER BY "title" ASC'
     let meta = (await db.query(query, [psid])).rows
-    query = 'SELECT DISTINCT ON ("solution"."uid", "problem"."title") "solution"."sid", "solution"."score", "solution"."uid", "user"."nickname", "solution"."pid", "solution"."when" FROM "solution" INNER JOIN "user" ON "solution"."uid" = "user"."uid" INNER JOIN "problem" ON "solution"."pid" = "problem"."pid" INNER JOIN "problemset" ON "problem"."psid" = "problemset"."psid" WHERE "problem"."psid" = $1 AND "solution"."when" <@ "problemset"."during" AND ("problemset"."secret_time" ISNULL OR NOT "solution"."when" <@ "problemset"."secret_time") ORDER BY "solution"."uid" ASC, "problem"."title" ASC, "solution"."score" DESC'
+    query = 'SELECT DISTINCT ON ("solution"."uid", "problem"."title") "solution"."sid", "solution"."score", ("solution"."score" >= 100) AS "pass", "solution"."uid", "user"."nickname", "solution"."pid", "solution"."when" FROM "solution" INNER JOIN "user" ON "solution"."uid" = "user"."uid" INNER JOIN "problem" ON "solution"."pid" = "problem"."pid" INNER JOIN "problemset" ON "problem"."psid" = "problemset"."psid" WHERE "problem"."psid" = $1 AND "solution"."when" <@ "problemset"."during" AND ("problemset"."secret_time" ISNULL OR NOT "solution"."when" <@ "problemset"."secret_time") ORDER BY "solution"."uid" ASC, "problem"."title" ASC, "solution"."score" DESC'
     let ret = (await db.query(query, [psid])).rows
     let tab = []
     for (let row of ret) {
@@ -61,6 +61,7 @@ router.get('/id/:psid(\\d+)/rank', lc,
       urow.detail.push({
         'pid': row.pid,
         'score': row.score,
+        'pass': row.pass,
         'sid': row.sid,
         'when': row.when
       })
