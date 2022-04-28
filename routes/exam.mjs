861e0b1b3d503477ac55578d6469542f3d33c62f
ArcanusNEO <lucasding@foxmail.com>
2022-04-28 15:59:15 +0800

    .

--- routes/exam.mjs
+++ routes/exam.mjs
@@ -45,7 +45,7 @@ router.get('/id/:psid(\\d+)/rank', lc,
     let psid = parseInt(req.params.psid)
     let query = 'SELECT "pid", "title" FROM "problem" WHERE "psid" = $1 ORDER BY "title" ASC'
     let meta = (await db.query(query, [psid])).rows
-    query = 'SELECT DISTINCT ON ("solution"."uid", "problem"."title") "solution"."sid", "solution"."score", ("solution"."score" >= 100) AS "pass", "solution"."uid", "user"."nickname", "solution"."pid", "solution"."when" FROM "solution" INNER JOIN "user" ON "solution"."uid" = "user"."uid" INNER JOIN "problem" ON "solution"."pid" = "problem"."pid" INNER JOIN "problemset" ON "problem"."psid" = "problemset"."psid" WHERE "problem"."psid" = $1 AND "solution"."when" <@ "problemset"."during" AND ("problemset"."secret_time" ISNULL OR NOT "solution"."when" <@ "problemset"."secret_time") ORDER BY "solution"."uid" ASC, "problem"."title" ASC, "solution"."score" DESC'
+    query = 'SELECT DISTINCT ON ("solution"."uid", "problem"."title") "solution"."sid", "solution"."score", ("solution"."score" >= 100) AS "pass", "solution"."uid", "user"."nickname", "solution"."pid", "solution"."run_time" AS "runTime", "solution"."run_memory" AS "runMemory", "solution"."when" FROM "solution" INNER JOIN "user" ON "solution"."uid" = "user"."uid" INNER JOIN "problem" ON "solution"."pid" = "problem"."pid" INNER JOIN "problemset" ON "problem"."psid" = "problemset"."psid" WHERE "problem"."psid" = $1 AND "solution"."when" <@ "problemset"."during" AND ("problemset"."secret_time" ISNULL OR NOT "solution"."when" <@ "problemset"."secret_time") ORDER BY "solution"."uid" ASC, "problem"."title" ASC, "solution"."score" DESC, "solution"."run_time" ASC, "solution"."run_memory" ASC, "solution"."when" ASC'
     let ret = (await db.query(query, [psid])).rows
     let tab = []
     for (let row of ret) {
@@ -54,15 +54,23 @@ router.get('/id/:psid(\\d+)/rank', lc,
           'uid': row.uid,
           'nickname': row.nickname,
           'totScore': 0,
+          'totTime': 0,
+          'totMem': 0,
+          'lastPassTime': new Date('2000-01-01 00:00:00+00'),
           'detail': []
         })
       let urow = tab[tab.length - 1]
       urow.totScore += row.score
+      urow.totTime += row.runTime
+      urow.totMem += row.runMemory
+      urow.lastPassTime = (new Date(row.when) > urow.lastPassTime ? row.when : urow.lastPassTime)
       urow.detail.push({
         'pid': row.pid,
         'score': row.score,
         'pass': row.pass,
         'sid': row.sid,
+        'time': row.runTime,
+        'memory': row.runMemory,
         'when': row.when
       })
     }
