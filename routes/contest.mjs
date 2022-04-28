c886eea3eb1984b721fd3ff727a65b7bc65ada8c
ArcanusNEO <lucasding@foxmail.com>
2022-04-28 20:21:51 +0800

    acm rank without unblock self

--- routes/contest.mjs
+++ routes/contest.mjs
@@ -4,6 +4,7 @@ import express from 'express'
 const router = express.Router()
 import lc from './midwares/login-check.mjs'
 import mc from './midwares/member-check.mjs'
+import db from '../utils/database.mjs'
 
 router.get('/', lc,
   async (req, res, next) => {
@@ -34,4 +35,53 @@ router.get('/id/:psid(\\d+)', lc,
   async (req, res, next) => {
     return (mc['problemset'](req.tokenAcc.uid, req.params.psid)(req, res, next))
   }, getDetail)
+
+router.get('/id/:psid(\\d+)/rank', lc,
+  async (req, res, next) => {
+    return (mc['problemset'](req.tokenAcc.uid, req.params.psid)(req, res, next))
+  },
+  async (req, res) => {
+    let psid = parseInt(req.params.psid)
+    let query = 'SELECT "pid", "title" FROM "problem" WHERE "psid" = $1 ORDER BY "title" ASC'
+    let meta = (await db.query(query, [psid])).rows
+    query = 'SELECT LOWER("problemset"."during")::TIMESTAMPTZ AS "begin", ("problemset"."secret_time" NOTNULL AND NOW()::TIMESTAMPTZ <@ "problemset"."secret_time") AS "secret" FROM "problemset" WHERE "problemset"."psid" = $1'
+    let setInfo = (await db.query(query, [psid])).rows[0]
+    if (setInfo.secret) query = 'SELECT DISTINCT ON ("solution"."uid", "problem"."title") "solution"."sid", ("solution"."score" >= 100) AS "pass", "solution"."uid", "user"."nickname", "solution"."pid", "solution"."when", (SELECT COUNT(*) FROM "solution" AS "inner_sol" WHERE "inner_sol"."uid" = "solution"."uid" AND "solution"."pid" = "inner_sol"."pid" AND "inner_sol"."when" < "solution"."when" AND ("problemset"."secret_time" ISNULL OR NOT "inner_sol"."when" <@ "problemset"."secret_time")) AS "tryCount", ("solution"."when" - LOWER("problemset"."during")::TIMESTAMPTZ) AS "realTime" FROM "solution" INNER JOIN "user" ON "solution"."uid" = "user"."uid" INNER JOIN "problem" ON "solution"."pid" = "problem"."pid" INNER JOIN "problemset" ON "problem"."psid" = "problemset"."psid" WHERE "problem"."psid" = $1 AND "solution"."when" <@ "problemset"."during" AND ("problemset"."secret_time" ISNULL OR NOT "solution"."when" <@ "problemset"."secret_time") ORDER BY "solution"."uid" ASC, "problem"."title" ASC, "solution"."score" DESC, "solution"."when" ASC'
+    else query = 'SELECT DISTINCT ON ("solution"."uid", "problem"."title") "solution"."sid", ("solution"."score" >= 100) AS "pass", "solution"."uid", "user"."nickname", "solution"."pid", "solution"."when", (SELECT COUNT(*) FROM "solution" AS "inner_sol" WHERE "inner_sol"."uid" = "solution"."uid" AND "solution"."pid" = "inner_sol"."pid" AND "inner_sol"."when" < "solution"."when") AS "tryCount", ("solution"."when" - LOWER("problemset"."during")::TIMESTAMPTZ) AS "realTime" FROM "solution" INNER JOIN "user" ON "solution"."uid" = "user"."uid" INNER JOIN "problem" ON "solution"."pid" = "problem"."pid" INNER JOIN "problemset" ON "problem"."psid" = "problemset"."psid" WHERE "problem"."psid" = $1 AND "solution"."when" <@ "problemset"."during" ORDER BY "solution"."uid" ASC, "problem"."title" ASC, "solution"."score" DESC, "solution"."when" ASC'
+    let ret = (await db.query(query, [psid])).rows
+    let tab = []
+    for (let row of ret) {
+      if (tab.length === 0 || row.uid !== tab[tab.length - 1].uid)
+        tab.push({
+          'uid': row.uid,
+          'nickname': row.nickname,
+          'passCount': 0,
+          'failCount': 0,
+          'virtTime': 0,
+          'detail': []
+        })
+      let urow = tab[tab.length - 1]
+      if (row.pass) {
+        urow.passCount += 1
+        uraw.failCount += row.tryCount
+        uraw.virtTime += (20 * 60 * 1000) * row.tryCount + row.realTime
+      }
+      urow.detail.push({
+        'pid': row.pid,
+        'pass': row.pass,
+        'sid': row.sid,
+        'when': row.when,
+        'realTime': row.realTime,
+        'tryCount': row.tryCount + (row.pass ? 0 : 1)
+      })
+    }
+    tab.sort((a, b) => {
+      if (a.passCount !== b.passCount) return b.passCount - a.passCount
+      if (a.virtTime !== b.virtTime) return a.virtTime - b.virtTime
+      return a.uid - b.uid
+    })
+    return res.status(hsc.ok).json({ meta, tab })
+  }
+)
+
 export default router
