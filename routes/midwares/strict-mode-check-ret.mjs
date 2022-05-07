a1d3069a436a944a8c2b4cc7c4dfdd1992532ae3
ArcanusNEO <lucasding@foxmail.com>
2022-05-07 14:43:31 +0800

    .

--- routes/midwares/strict-mode-check-ret.mjs
+++ routes/midwares/strict-mode-check-ret.mjs
@@ -1,8 +1,10 @@
 import fs from 'fs-extra'
+import basic from '../../config/basic.mjs'
+import path from 'path'
 
 export default async () => {
   try {
-    let file = await fs.readFile('../../config/strict-mode.json', 'utf8')
+    let file = await fs.readFile(path.resolve(basic.dataBase, 'strict-mode.json'), 'utf8')
     let mode = JSON.parse(file)
     if (!mode || !mode.enable) throw Error('Strict mode off')
     return mode.code
