417d30501f538f06abb4f9f7a437e5b8c5d7b8ab
ArcanusNEO <lucasding@foxmail.com>
2022-05-13 20:25:37 +0800

    .

--- routes/admin/user.mjs
+++ routes/admin/user.mjs
@@ -7,19 +7,24 @@ import pc from '../midwares/permission-check.mjs'
 
 router.get('/', lc,
   async (req, res, next) => {
-    return pc(req.tokenAcc.uid, ['master'])(req, res, next)
+    if (!(req.tokenAcc.permission >= 1)) return res.sendStatus(hsc.forbidden)
+    return next()
   },
   async (req, res) => {
-    let { uid, gid, nickname, email, qq, tel, realname, school, words, limit } = req.query
-    let queryStr = 'SELECT * FROM "user" INNER JOIN "group" ON "user"."gid" = "group"."gid" WHERE NOT "user"."removed"'
+    let { uid, gid, nickname, email, qq, tel, realname, school, words } = req.query
+    let queryStr = 'SELECT "uid", "user"."gid", "title" AS "group", "description" AS "gdesc", "perm", "nickname", "email", "qq", "tel", "realname", "school", "words", "signup_time" AS "signupTime", "removed" FROM "user" INNER JOIN "group" ON "user"."gid" = "group"."gid" WHERE TRUE'
     let allParams = { uid, gid, nickname, email, qq, tel, realname, school, words }
     let param = []
     for (let key in allParams) {
       if (!allParams[key]) continue
-      console.log(allParams[key])
       queryStr += ` AND "user"."${key}" = $${param.push(allParams[key])}`
     }
-    if (parseInt(limit) > 0) queryStr += ` LIMIT $${param.push(parseInt(limit))}`
+    let page = parseInt(req.query.page), item = parseInt(req.query.item)
+    let limit = item, offset = (page - 1) * item
+    if (limit > 0) {
+      queryStr += ` LIMIT $${param.push(parseInt(limit))}`
+      if (offset >= 0) queryStr += ` OFFSET $${param.push(offset)}`
+    }
     let ret = (await db.query(queryStr, param)).rows
     return res.status(hsc.ok).json(ret)
   })
@@ -27,10 +32,11 @@ router.get('/', lc,
 
 router.get('/id/:uid(\\d+)', lc,
   async (req, res, next) => {
-    return pc(req.tokenAcc.uid, ['master'])(req, res, next)
+    if (!(req.tokenAcc.permission >= 1)) return res.sendStatus(hsc.forbidden)
+    return next()
   },
   async (req, res) => {
-    let query = 'SELECT * FROM "user" WHERE "uid" = $1'
+    let query = 'SELECT "uid", "user"."gid", "title" AS "group", "description" AS "gdesc", "perm", "nickname", "email", "password", "qq", "tel", "realname", "school", "words", "signup_time" AS "signupTime", "removed" FROM "user" INNER JOIN "group" ON "user"."gid" = "group"."gid" WHERE "uid" = $1'
     let ret = (await db.query(query, [req.params.uid])).rows[0]
     return res.status(hsc.ok).json(ret)
   })
